888d9ff108bf8fd63f3be170b681dfe8834649b7
samson <sam.sunny@a-cti.com>
2017-07-01 12:47:22 +0530

    gave multiple position to display the collection view cells.

--- remind me/CustomCollectionViewLayout.swift
+++ remind me/CustomCollectionViewLayout.swift
@@ -19,14 +19,74 @@ class CustomCollectionViewLayout: UICollectionViewLayout {
 		
 		if cache.isEmpty {
 			
-			for item in 0 ..< collectionView!.numberOfItems(inSection: 0) {
+//			for section in 0 ..< collectionView!.numberOfSections {
+				for item in 0 ..< collectionView!.numberOfItems(inSection: 0) {
+					
+					
+					switch item {
+					case 0:
+						let indexPath = NSIndexPath(item: item, section: 0)
+						let frame = CGRect(x: 100, y: 150, width: 50.0, height: 100.0)
+						let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+						attributes.frame = frame
+						cache.append(attributes)
+						
+					case 1:
+						let indexPath = NSIndexPath(item: item, section: 0)
+						let frame = CGRect(x: 30, y: 200, width: 50.0, height: 100.0)
+						let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+						attributes.frame = frame
+						cache.append(attributes)
+					
+					case 2:
+						let indexPath = NSIndexPath(item: item, section: 0)
+						let frame = CGRect(x: 150, y: 300, width: 50.0, height: 100.0)
+						let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+						attributes.frame = frame
+						cache.append(attributes)
+					
+					case 3:
+						let indexPath = NSIndexPath(item: item, section: 0)
+						let frame = CGRect(x: 200, y: 600, width: 50.0, height: 100.0)
+						let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+						attributes.frame = frame
+						cache.append(attributes)
+						
+					
+					default:
+						let indexPath = NSIndexPath(item: item, section: 0)
+						let frame = CGRect(x: 0, y: 0, width: 50.0, height: 300.0)
+						let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+						attributes.frame = frame
+						cache.append(attributes)
+					}
+					
+					
+					
+//					if item == 3 {
+//						let indexPath = NSIndexPath(item: item, section: 0)
+//						let frame = CGRect(x: 0, y: 150, width: 50.0, height: 100.0)
+//						let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+//						attributes.frame = frame
+//						cache.append(attributes)
+//					} else {
+//						let indexPath = NSIndexPath(item: item, section: 0)
+//						let frame = CGRect(x: 0, y: 0, width: 50.0, height: 100.0)
+//						let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+//						attributes.frame = frame
+//						cache.append(attributes)
+//					}
+				}
 				
-				let indexPath = NSIndexPath(item: item, section: 0)
-				let frame = CGRect(x: 0, y: 0, width: 50.0, height: 100.0)
-				let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
-				attributes.frame = frame
-				cache.append(attributes)
-			}
+//				for item in 0 ..< collectionView!.numberOfItems(inSection: 1) {
+//					
+//					let indexPath = NSIndexPath(item: item, section: 0)
+//					let frame = CGRect(x: 0, y: 150, width: 50.0, height: 100.0)
+//					let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
+//					attributes.frame = frame
+//					cache.append(attributes)
+//				}
+//			}
 		}
 	}
 	
